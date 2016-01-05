9ff9425557e975c0862d4494dbeeb894fa7ae82c
DanielsonSilva <danielson_fx@yahoo.com.br>
2016-01-05 18:01:19 -0300

    Tool Bar created and JFreeChart library added
    #1 - Add the tool bar
    #2 - Add the menu items
    #3 - Update the size of the screen
    #4 - Add the JFreeChart libraries
    #5 - Add icons for the tool buttons in the resources folder
    #6 - Add more words in MessagesBundle
    TODO: Create a Chart in the middle of the screen

--- Project/src/view/Principal.java
+++ Project/src/view/Principal.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -7,10 +9,14 @@ import java.awt.Toolkit;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
 
 public class Principal extends JFrame {
 
@@ -24,7 +30,7 @@ public class Principal extends JFrame {
         // Initialize variables
         String title = messages.getString("title");
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        sizeRate = 0.7;
+        sizeRate = 0.9;
         width  = (int) (Math.round(dim.width*sizeRate));
         height = (int) (Math.round(dim.height*sizeRate));
 
@@ -40,11 +46,13 @@ public class Principal extends JFrame {
         // Title of the window
         this.setTitle(title);
         // Border Layout for the components
-        this.setLayout(new GridBagLayout());
+        this.setLayout(new BorderLayout());
         // Initialize the window in the center of the monitor
         this.setLocationRelativeTo(null);  
         // Add the menu bar
         addMenuBarToPane(this,messages);
+        // Add the tool bar
+        addToolBarToPane(this,messages);
         // Pop-up the main window
         this.setVisible(true);
     }
@@ -63,13 +71,101 @@ public class Principal extends JFrame {
         // Setting the menu categories
         JMenu fileMenu = new JMenu(messages.getString("MenuFile"));
         JMenu helpMenu = new JMenu(messages.getString("MenuHelp"));
+        JMenu toolMenu = new JMenu(messages.getString("MenuTools"));
         menuBar.add(fileMenu);
+        menuBar.add(toolMenu);
         menuBar.add(helpMenu);
         // Setting the menu items
         JMenuItem exitAction = new JMenuItem(messages.getString("MenuItemExit"));
         JMenuItem aboutAction = new JMenuItem(messages.getString("MenuItemAbout"));
+        JMenuItem helpAction = new JMenuItem(messages.getString("MenuItemHelp"));
+        JMenuItem ConfigurationAction = new JMenuItem(messages.getString("MenuItemConfiguration"));
         // Setting up the adding
         fileMenu.add(exitAction);
+        toolMenu.add(ConfigurationAction);
         helpMenu.add(aboutAction);
+        helpMenu.add(helpAction);
+    }
+    
+    /**
+     * Add a tool bar to the program
+     * 
+     * @param frame Container that will receive the components
+     * @param messages Variable for internationalization
+     */    
+    private void addToolBarToPane(JFrame frame, ResourceBundle messages)
+    {
+    	// Create the tool bar
+    	JToolBar toolBar = new JToolBar();
+    	toolBar.setFloatable(false);
+    	toolBar.setRollover(true);
+    	Container contentPane = frame.getContentPane();
+    	contentPane.add(toolBar, BorderLayout.NORTH);
+    	
+    	JButton button = null;
+    	Icon icone = null;
+    	// Play button
+    	icone = new ImageIcon(getClass().getResource("/resources/play.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        // Pause button
+    	icone = new ImageIcon(getClass().getResource("/resources/pause.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        // Stop button
+    	icone = new ImageIcon(getClass().getResource("/resources/stop.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        
+        // Add a separator
+        toolBar.addSeparator();
+        
+        // First Cycle
+        icone = new ImageIcon(getClass().getResource("/resources/first.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        // Previous Cycle
+        icone = new ImageIcon(getClass().getResource("/resources/previous.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        // Next Cycle
+        icone = new ImageIcon(getClass().getResource("/resources/next.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        // Last Cycle
+        icone = new ImageIcon(getClass().getResource("/resources/last.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
+        
+        // Add a Separator
+        toolBar.addSeparator();
+        
+        // Configuration
+        icone = new ImageIcon(getClass().getResource("/resources/configuration.png"));
+        button = new JButton();
+        button.setIcon(icone);
+        toolBar.add(button);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
